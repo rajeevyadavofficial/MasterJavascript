a0b801d2ad959013c315fdddc46e9d6947bb9001
Rajeev Yadav <94947701+rajeevyadavofficial@users.noreply.github.com>
2024-10-24 17:28:45 +0000

    variable concept

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const age = 21
+let name1 = 'rajeev'
+var surname = 'yadav'
+
+var surname = '22'
+
+{
+    let name1 = "rajeevyadaav"
+    console.log(surname)
+}
+
+email = "rajeev@gmail.com"
+name1 = "apple"
+console.log(name1)
+console.table([name1,email,age])
